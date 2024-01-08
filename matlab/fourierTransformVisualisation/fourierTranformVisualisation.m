7066c2b15745a8eedc3909b1b366c3b46b0d653d
Swapnil Tyagi <swapniltyagianfield@gmail.com>
2024-01-08 21:50:14 +0530

    Adding ananlysis of square pulse and sinc pulse

--- matlab/fourierTransformVisualisation/fourierTranformVisualisation.m
+++ matlab/fourierTransformVisualisation/fourierTranformVisualisation.m
@@ -4,12 +4,19 @@ clear;
 close all;
 %% sinusoidal signal
 
-L = 100; %length of signal
+L = 1000; %length of signal
 Ts = 1/50;
-t = (0:1:L-1)*Ts;
+%t = (0:1:L-1)*Ts;
+t = (-L/2:1:L/2-1)*Ts;
 f1 = 20; %  MhZ FREQUENCY
 f2 = 15; 
-y = 20 + sin(2*pi*f1*t) + cos(2*pi*f2*t);
+
+%y = zeros(1,L);
+%y = 2 + sin(2*pi*f1*t) + cos(2*pi*f2*t); %sinusiodal signal
+%y(1:40) = 1; %square pulse
+y = sin(2*pi*f1*t)./(2*pi*f1*t); %sampling function
+y(isnan(y)) = 1;
+
 
 fs = 1/Ts;
 f = (-L/2:1:L/2-1)*(fs/L); 
@@ -20,7 +27,7 @@ figure(1)
 subplot(121);
 plot(t,y);
 xlabel("t");
-ylabel("sin(2*pi*f*t)");
+ylabel("y");
 grid on
 title("sinusoidal signal")
 
